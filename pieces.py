990736088f19008d9cf0b8a5a72f4d1789cf07dc
Omar Gatica <omar.gatica.017@gmail.com>
2020-12-12 16:45:46 -0800

    Fixed pawn moves logic.

--- pieces.py
+++ pieces.py
@@ -3,66 +3,71 @@ class Piece:
         self.color = color
         self.position = (i, j)
         self.firstMove = True
-    def valid_moves(self, board):
-    """Returns a list of valid coordinates for respective piece."""
+
+    def valid_moves(
+        self, board
+    ):  # Returns a list of valid coordinates for respective piece
         pass
+
     def inside_board(self, i: int, j: int) -> bool:
-        if square[0] < 8 and square[1] < 8:
+        if i < 8 and j < 8:
             return True
         return False
 
+
 class Pawn(Piece):
     def valid_moves(self, board: list[list[Piece]]):
-        i, j = self.position  
-        coordinates = []          
-        if self.color == 'w':
-            #Check front moves
-            if self.firstMove and not board[i + 2][j]:
-                coordinates.append((i + 2, j))
-            if self.insideBoard((i + 1, j)) and not board[i + 1][j]:
-                coordinates.append((i + 1, j))
-            #Check left diagonal
-            if self.insideBoard(i + 1, j - 1):
-                if not board[i + 1][j - 1] or board[i + 1][j - 1].color == 'b':
-                    coordinates.append((i + 1, j - 1))
-            #Check right diagonal
-            if self.insideBoard(i + 1, j + 1):
-                if not board[i + 1][j + 1] or board[i + 1][j + 1].color == 'b':
-                    coordinates.append((i + 1, j - 1))
+        i, j = self.position
+        coordinates = []
+        if self.color == Color.WHITE:
+            op = lambda a, b: a + b
         else:
-            #Check front moves
-            if self.firstMove and not board[i - 2][j]:
-                coordinates.append((i - 2, j))
-            if self.insideBoard((i - 1, j)) and not board[i - 1][j]:
-                coordinates.append((i - 1, j))
-            #Check left diagonal
-            if self.insideBoard(i - 1, j + 1):
-                if not board[i - 1][j + 1] or board[i - 1][j + 1].color == 'w':
-                    coordinates.append((i - 1, j + 1))
-            #Check right diagonal
-            if self.insideBoard(i - 1, j - 1):
-                if not board[i - 1][j - 1] or board[i - 1][j - 1].color == 'w':
-                    coordinates.append((i - 1, j - 1))
+            op = lambda a, b: a - b
+            # Check front moves
+            if self.firstMove and not board[op(i, 2)][j]:
+                coordinates.append(str((op(i, 2) + 1), str(board.inv_col[j])))
+            if self.inside_board(op(i, 1), j) and not board[op(i, 1)][j]:
+                coordinates.append((str(op(i, 1) + 1), str(board.inv_col[j])))
+            # Check left diagonal
+            if self.inside_board(op(i, 1), j - 1):
+                if (
+                    not board[op(i, 1)][j - 1]
+                    or board[op(i, 1)][j - 1].color == Color.BLACK
+                ):
+                    coordinates.append((str(op(i, 1) + 1), str(board.inv_col[j - 1])))
+            # Check right diagonal
+            if self.inside_board(op(i, 1), j + 1):
+                if (
+                    not board[op(i, 1)][j + 1]
+                    or board[op(i, 1)][j + 1].color == Color.BLACK
+                ):
+                    coordinates.append((str(op(i, 1) + 1), str(board.inv_col[j - 1])))
         return coordinates
-        
+
     def promote(self):
         pass
+
 
 class Rook(Piece):
     def valid_moves(self, board):
         pass
 
+
 class Knight(Piece):
     def valid_moves(self, board):
         pass
+
 
 class Bishop(Piece):
     def valid_moves(self, board):
         pass
 
+
 class Queen(Piece):
     def valid_moves(self, board):
         pass
+
+
 class King(Piece):
-    def valid_moves(self, board):  
+    def valid_moves(self, board):
         pass
