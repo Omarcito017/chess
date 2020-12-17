d9c75babbe103dcc8cb35aff99527ebdbc7e7d3d
Omar Gatica <omar.gatica.017@gmail.com>
2020-12-17 13:13:51 -0800

    Fixed circular dependency and implemented pawn moves.

--- board.py
+++ board.py
@@ -15,21 +15,20 @@ class Board:
             i, j = 1, 0
         # Initialize pawns
         for k in range(8):
-            for letter in self.columns.keys():
-                self.matrix[i][k] = Pawn(color, letter + str(i + 1))
-                break
+            self.matrix[i][k] = Pawn(color)
+
         # Rooks
-        self.matrix[j][0] = Rook(color, "H" + str(j + 1))
-        self.matrix[j][7] = Rook(color, "A" + str(j + 1))
+        self.matrix[j][0] = Rook(color)
+        self.matrix[j][7] = Rook(color)
         # Knights
-        self.matrix[j][1] = Knight(color, "G" + str(j + 1))
-        self.matrix[j][6] = Knight(color, "B" + str(j + 1))
+        self.matrix[j][1] = Knight(color)
+        self.matrix[j][6] = Knight(color)
         # Bishops
-        self.matrix[j][2] = Bishop(color, "F" + str(j + 1))
-        self.matrix[j][5] = Bishop(color, "C" + str(j + 1))
+        self.matrix[j][2] = Bishop(color)
+        self.matrix[j][5] = Bishop(color)
         # Royalty
-        self.matrix[j][3] = Queen(color, "E" + str(j + 1))
-        self.matrix[j][4] = King(color, "D" + str(j + 1))
+        self.matrix[j][3] = Queen(color)
+        self.matrix[j][4] = King(color)
 
     def __getitem__(self, pos: str) -> Piece:
         row = int(pos[1]) - 1
@@ -41,7 +40,52 @@ class Board:
         column = self.columns[pos[0]]
         self.matrix[row][column] = item
 
+    def inside_board(self, i: int, j: int):
+        return 0 <= i < 8 and 0 <= j < 8
+
     def indeces_to_coordinates(self, i: int, j: int) -> str:
         row = str(i + 1)
-        col = self.columns[j]
+        col = self.inv_col[j]
+        print(col + row)
         return col + row
+
+    def coordinates_to_indeces(self, coord: str) -> tuple:
+        i, j = int(coord[1]) - 1, self.columns[coord[0]]
+        return (i, j)
+
+    def valid_moves(self, position: str) -> list[str]:
+        curr_piece = self[position]
+        print("Good")
+        if isinstance(curr_piece, Pawn):
+            print("Went inside")
+            return self.pawn_valid_moves(position, curr_piece)
+
+    def pawn_valid_moves(self, position: str, curr_piece: Piece) -> list[str]:
+        def add(a, b):
+            return a + b
+
+        def sub(a, b):
+            return a - b
+        print("got called")
+        # Integer values to index the matrix
+        i, j = self.coordinates_to_indeces(position)
+        coordinates = []
+        if curr_piece.color == Color.WHITE:
+            op = sub
+        else:
+            op = add
+        # Check front moves
+        if curr_piece.firstMove and not self.matrix[op(i, 2)][j]:
+            coordinates.append(self.indeces_to_coordinates(op(i, 2), j))
+        if self.inside_board(op(i, 1), j) and not self.matrix[op(i, 1)][j]:
+            coordinates.append(self.indeces_to_coordinates(op(i, 1), j))
+        # Check Left Diagonal
+        if self.inside_board(op(i, 1), j - 1) and self.matrix[op(i, 1)][j - 1]:
+            if self.matrix[op(i, 1)][j - 1].color != curr_piece.color:
+                coordinates.append(self.indeces_to_coordinates(op(i, 1), j - 1))
+        # Check Right Diagonal
+        if self.inside_board(op(i, 1), j + 1) and self.matrix[op(i, 1)][j + 1]:
+            if self.matrix[op(i, 1)][j + 1].color != curr_piece.color:
+                coordinates.append(self.indeces_to_coordinates(op(i, 1), j + 1))
+        print(coordinates)
+        return coordinates
--- pieces.py
+++ pieces.py
@@ -1,72 +1,32 @@
 from color import Color
-from board import Board
 from abc import ABC
 
 
 class Piece(ABC):
-    def __init__(self, color: Color, position: str):
+    def __init__(self, color: Color):
         self.color = color
-        self.position = position
         self.firstMove = True
-
-    def valid_moves(self, board: Board):
-        pass
-
-    def inside_board(self, i: int, j: int):
-        return 0 < i < 8 and 0 < j < 8
 
 
 class Pawn(Piece):
-    def valid_moves(self, board: Board):
-        def add(a, b):
-            return a + b
-
-        def sub(a, b):
-            return a - b
-
-        # Integer values to index the matrix
-        i, j = board.coordinates_to_indeces(self.position)
-        coordinates = []
-        if self.color == Color.WHITE:
-            op = add
-        else:
-            op = sub
-        # Check front moves
-        if self.firstMove and not board.matrix[op(i, 2)][j]:
-            coordinates.append(board.indeces_to_coordinates(op(i, 2), j))
-        if self.inside_board(op(i, 1), j) and not board.matrix[op(i, 1)][j]:
-            coordinates.append(board.indeces_to_coordinates(op(i, 1), j))
-        # Check Left Diagonal
-        if self.inside_board(op(i, 1), j - 1) and board.matrix[op(i, 1)][j - 1]:
-            if board.matrix[op(i, 1)][j - 1].color != self.color:
-                coordinates.append(board.indeces_to_coordinates(op(i, 1), j - 1))
-        # Check Right Diagonal
-        if self.inside_board(op(i, 1), j + 1) and board.matrix[op(i, 1)][j + 1]:
-            if board.matrix[op(i, 1)][j + 1].color != self.color:
-                coordinates.append(board.indeces_to_coordinates(op(i, 1), j + 1))
-        return coordinates
+    pass
 
 
 class Rook(Piece):
-    def valid_moves(self):
-        pass
+    pass
 
 
 class Knight(Piece):
-    def valid_moves(self):
-        pass
+    pass
 
 
 class Bishop(Piece):
-    def valid_moves(self):
-        pass
+    pass
 
 
 class Queen(Piece):
-    def valid_moves(self):
-        pass
+    pass
 
 
 class King(Piece):
-    def valid_moves(self):
-        pass
+    pass
